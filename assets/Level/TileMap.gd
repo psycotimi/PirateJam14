c748b25f7d5f05f91bbcd71d5eb5b4f4f7de5444
ropo <gitlab@roponkolo.fi>
2024-01-13 15:06:44 +0200

    automagiamaalaust pelaa, gdscriptissä nimellä terrain_set

--- assets/Level/TileMap.gd
+++ assets/Level/TileMap.gd
@@ -1,22 +1,22 @@
 extends TileMap
 
 
-var gridsize = 32
-var alltiles = {}
+var gridSize = 32
+var allTiles = {}
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-	for x in gridsize:
-		for y in gridsize:
-			alltiles[str(Vector2(x,y))] = {
-				"surface": "bread"
-			}
-			set_cell(0, Vector2(x,y), 1, Vector2(1,1),0)
+    for x in gridSize:
+        for y in gridSize:
+            allTiles[str(Vector2(x,y))] = {
+                "surface": "bread"
+            }
+            set_cell(0, Vector2(x,y), 1, Vector2(1,1),0)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	var tile = local_to_map(get_global_mouse_position())
+    var tile = local_to_map(get_global_mouse_position())
 
-	if alltiles.has(str(tile)):
-		set_cell(1, tile,1,Vector2i(0,0),0)
+    if allTiles.has(str(tile)):
+        set_cell(0, tile,1,Vector2i(0,0),0)
