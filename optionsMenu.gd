620608b03b80b5281740581f5c5cd8fd102914b6
psycoma <timi_salminen@hotmail.com>
2024-01-16 12:19:18 +0200

    mute mutettaa

--- optionsMenu.gd
+++ optionsMenu.gd
@@ -1,4 +1,11 @@
 extends Control
 
+# Nappulat joilla palataan main menuun ja mutetaan kaikki äänet
+
 func _on_back_pressed():
-	get_tree().change_scene_to_file("res://menu.tscn")
+    get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_mute_pressed():
+    var bus_idx = AudioServer.get_bus_index("Master")
+    AudioServer.set_bus_mute(bus_idx, true) # or false
