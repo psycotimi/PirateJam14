dfe6be527c4dac46b38e4d33cfe9c81ba4f8cf8f
timi salminen <salminen.timi@gmail.com>
2024-01-17 18:53:35 +0200

    toimiva gridi

--- assets/Level/gamegrid.gd
+++ assets/Level/gamegrid.gd
@@ -8,8 +8,8 @@ var troopLabel = Label.new()
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
-    for x in gridSize:
-        for y in gridSize:
+    for x in range(23, gridSize+23):
+        for y in range(12, gridSize+12):
             troopLabel.position = Vector2(x,y)
             var legalMoves = [
                 Vector2(x+1,y),
@@ -23,8 +23,8 @@ func _ready():
                 Vector2(2*x+1,2*y),
                 Vector2(2*x+1,2*y+1)
                 ]
-            tiles[Vector2(x,y)] = {
-                "surface" : Global.spreadTypeList[2], # aluksi hilloton
+            tiles[str(Vector2(x,y))] = {
+                "surface" : Global.spreadTypeList[1], # aluksi hilloton
                 "troops" : 0, # joukkojen lukumäärä tilellä, ehkä pitää siirtää alueeseen sitku semmonen on
                 "areaid" : 0, # tätä voi käyttää myöhemmin
                 "grafiikkatilet" : grafiikkatilet,
@@ -37,9 +37,10 @@ func _ready():
 func _process(delta):
     var tile = local_to_map(get_global_mouse_position())
     
-    for x in gridSize:
-        for y in gridSize*20:
+    for x in range(23, gridSize+23):
+        for y in range(12, gridSize+12):
            erase_cell(1, Vector2(x,y))
             
     if tiles.has(str(tile)):
+        print(tile)
         set_cell(1, tile, 1, Vector2i(0,0), 0)
