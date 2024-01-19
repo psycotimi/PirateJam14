87e11e72d1888c805bc60159feb5041e6cb1a3ec
ropo <gitlab@roponkolo.fi>
2024-01-19 14:56:58 +0200

    lisätty alueet, vielä pitää siirtää gameplay toiminnallisuudet ruuduista alueisiin

--- assets/Level/gamegrid.gd
+++ assets/Level/gamegrid.gd
@@ -2,6 +2,9 @@ extends TileMap
 
 var gridSize = Global.gridsize
 var tiles = {}
+
+var alueet = {}
+
 var troopLabel = Label.new()
 var offsetX = 23
 var offsetY = 12
@@ -16,13 +19,6 @@ func _ready():
         for y in range(offsetY, gridSize+offsetY):
             troopLabel.position = Vector2(x,y)
 
-            # näistä pitää poistaa gridin ulkopuoliset ruudut
-            var legalMoves = [
-                Vector2(x+1,y),
-                Vector2(x-1,y),
-                Vector2(x,y+1),
-                Vector2(x,y-1)
-                ]
             var grafiikkatilet = [
                 Vector2(2*x,2*y),
                 Vector2(2*x,2*y+1),
@@ -34,14 +30,46 @@ func _ready():
                 "troops" : 0, # joukkojen lukumäärä tilellä, ehkä pitää siirtää alueeseen sitku semmonen on
                 "areaid" : 0, # tätä voi käyttää myöhemmin
                 "grafiikkatilet" : grafiikkatilet,
-                "legalmoves" : legalMoves
             }
             set_cell(0, Vector2i(x,y), 0, Vector2i(0,0),0)
     # print(tiles) #Testi, että dictionary tulostuu oikein
     alkupositio()
 
+
+    for x in range(0,8):
+        for y in range(0,8):
+            # näistä pitää vielä poistaa gridin ulkopuoliset ruudut
+            var legalmoves = [
+                Vector2(x+1,y),
+                Vector2(x-1,y),
+                Vector2(x,y+1),
+                Vector2(x,y-1)
+                ]
+            var ruudut = [
+                Vector2(offsetX+x,offsetY+y),
+                Vector2(offsetX+x+1,offsetY+y),
+                Vector2(offsetX+x,offsetY+y+1),
+                Vector2(offsetX+x+1,offsetY+y+1)
+                ]
+            alueet[str(Vector2i(x,y))] = {
+                "spread" : Global.spreadTypeList[0],
+                "troops" : 0,
+                "ruudut" : ruudut,
+                "legalmoves" : legalmoves
+            }
+            
+    # laillisten siirtojen laillisuuden tarkistus 
+    for alue in alueet:
+        for legalmove in alueet[alue].legalmoves:
+            if !alueet.has(str(legalmove)):
+                print(legalmove, " pitäisi poistaa")
+                var index = alueet[alue].legalmoves.find(legalmove)
+                alueet[alue].legalmoves.erase(legalmove)
+        print(alueet[alue].legalmoves)    
+        
+
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _process(_delta):
     var tile = local_to_map(get_global_mouse_position())
     
     for x in range(offsetX, gridSize+offsetX):
