01d423e43b244748df80bb3c7c33c72035a21ea6
Miika Hupli <miika.hupli@tuni.fi>
2024-01-22 10:11:35 +0200

    Lisää optins/mute pause menuun.

--- menut/pause_menu.gd
+++ menut/pause_menu.gd
@@ -1,5 +1,8 @@
 extends Control
 
+func _ready():
+    $Options.hide()
+    $Pause.show()
 
 # jatka peliä
 func _on_resume_pressed():
@@ -9,3 +12,30 @@ func _on_resume_pressed():
 # sulje peli
 func _on_quit_pressed():
    get_tree().quit()
+
+
+func _on_options_pressed():
+    $Pause.hide()
+    $Options.show()
+
+
+# nämä funktiot options menun sisällä
+func _on_mute_pressed():
+    if $Options/AnimatedSprite2D.get_frame() == 0:
+        
+        # mutee kaikki äänet
+        var bus_idx = AudioServer.get_bus_index("Master")
+        AudioServer.set_bus_mute(bus_idx, true) 
+        
+        # Vaihda kuvake
+        $Options/AnimatedSprite2D.set_frame(1)
+    
+    else:
+        $Options/AnimatedSprite2D.set_frame(0)
+        var bus_idx = AudioServer.get_bus_index("Master")
+        AudioServer.set_bus_mute(bus_idx, false)
+
+
+func _on_back_pressed():
+    $Options.hide()
+    $Pause.show()
