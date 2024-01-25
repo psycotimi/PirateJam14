9d2c3d5fd0a66f270c5bf853d65b5df42708d98f
SonjaOe <sonja.oehlandt@gmail.com>
2024-01-25 17:34:18 +0200

    revised overlong how to play plus added buttons

--- assets/UI/ui.gd
+++ assets/UI/ui.gd
@@ -9,37 +9,37 @@ extends CanvasLayer
 @onready var jamCount: Label = $JamTroopCounter/JamTroopCount
 
 func _ready():
-	$PauseMenu.hide()
+    $PauseMenu.hide()
 
 # Tällä voidaan päivittää vuoronumero Globaalin muuttujan avulla (turnNumber).
 func update_turn_counter():
-	turnNumber.text = str(Global.turnCounter + 1)
+    turnNumber.text = str(Global.turnCounter + 1)
 
 # Tällä voidaan päivittää vuorossa olevan pelaajan merkki Globaalin muuttujan
 # avulla (whoseTurn = "none" / "pb" / "jam").
 func update_turn_arrow():
-	if (Global.whoseTurn == "pb"):
-		pbArrow.show()
-		jamArrow.hide()
-	elif (Global.whoseTurn == "jam"):
-		pbArrow.hide()
-		jamArrow.show()
-	else:
-		pbArrow.hide()
-		jamArrow.hide()
+    if (Global.whoseTurn == "pb"):
+        pbArrow.show()
+        jamArrow.hide()
+    elif (Global.whoseTurn == "jam"):
+        pbArrow.hide()
+        jamArrow.show()
+    else:
+        pbArrow.hide()
+        jamArrow.hide()
 
 # Tällä voidaan yksinkertaisesti päivittää sotilaslaskurit.
 func update_troop_count():
-	pbCount.text = str(Global.pbTroopCount)
-	jamCount.text = str(Global.jamTroopCount)
+    pbCount.text = str(Global.pbTroopCount)
+    jamCount.text = str(Global.jamTroopCount)
 
 # Pausetetaan peli ja avataan pause menu.
 func _on_pause_button_pressed():
-	await buttonsound()
-	get_tree().paused = true
-	$PauseMenu.show()
+    await buttonsound()
+    get_tree().paused = true
+    $PauseMenu.show()
 
 func buttonsound():
-	$buttonsound.pitch_scale = randf_range(2,3)
-	$buttonsound.play()
-	await get_tree().create_timer(0.1).timeout
+    $buttonsound.pitch_scale = randf_range(2,3)
+    $buttonsound.play()
+    await get_tree().create_timer(0.1).timeout
--- menut/optionsMenu.gd
+++ menut/optionsMenu.gd
@@ -3,27 +3,27 @@ extends Control
 # Nappulat joilla palataan main menuun ja mutetaan kaikki äänet
 
 func _on_back_pressed():
-	await buttonsound()
-	get_tree().change_scene_to_file("res://menut/menu.tscn")
+    await buttonsound()
+    get_tree().change_scene_to_file("res://menut/menu.tscn")
 
 
 func _on_mute_pressed():
-	await buttonsound()
-	if $AnimatedSprite2D.get_frame() == 0:
-		
-		# mutee kaikki äänet
-		var bus_idx = AudioServer.get_bus_index("Master")
-		AudioServer.set_bus_mute(bus_idx, true) 
-		
-		# Vaihda kuvake
-		$AnimatedSprite2D.set_frame(1)
-	
-	else:
-		$AnimatedSprite2D.set_frame(0)
-		var bus_idx = AudioServer.get_bus_index("Master")
-		AudioServer.set_bus_mute(bus_idx, false)
+    await buttonsound()
+    if $AnimatedSprite2D.get_frame() == 0:
+        
+        # mutee kaikki äänet
+        var bus_idx = AudioServer.get_bus_index("Master")
+        AudioServer.set_bus_mute(bus_idx, true) 
+        
+        # Vaihda kuvake
+        $AnimatedSprite2D.set_frame(1)
+    
+    else:
+        $AnimatedSprite2D.set_frame(0)
+        var bus_idx = AudioServer.get_bus_index("Master")
+        AudioServer.set_bus_mute(bus_idx, false)
 
 func buttonsound():
-	$buttonsound.pitch_scale = randf_range(2,3)
-	$buttonsound.play()
-	await get_tree().create_timer(0.1).timeout
+    $buttonsound.pitch_scale = randf_range(2,3)
+    $buttonsound.play()
+    await get_tree().create_timer(0.1).timeout
