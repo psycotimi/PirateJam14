96eac123b3a1c23bf184dd1507c427de081a87a4
ropo <gitlab@roponkolo.fi>
2024-01-29 20:27:38 +0200

    lisätty play nappula how-to-playhin

--- ropon_how_to.gd
+++ ropon_how_to.gd
@@ -10,3 +10,8 @@ func buttonsound():
     $buttonsound.pitch_scale = 1.5
     $buttonsound.play()
     await get_tree().create_timer(0.1).timeout
+
+
+func _on_play_pressed():
+    await buttonsound()
+    get_tree().change_scene_to_file("res://assets/Level/TestGame/test_game.tscn")
